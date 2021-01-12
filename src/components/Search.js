95e03a4a1ed21e09578fb8364cf8f74adcfa8fe8
renatormsilva <renato.brtkm@gmail.com>
2021-01-12 17:11:47 -0400

    fix: href error

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,7 +5,7 @@ export default function Search() {
     return (
         <React.Fragment>
             <input className="search-txt" type="text" name="" placeholder="Enter a country name to get information about covid-19" />
-            <a className="search-btn" href="#">
+            <a className="search-btn" href="/#">
                 <i class="fas fa-search"></i>
             </a>
         </React.Fragment>
