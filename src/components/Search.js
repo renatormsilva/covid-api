28be19259e957234164c108e69c81f995150c1fb
renatormsilva <renato.brtkm@gmail.com>
2021-01-15 00:49:01 -0400

    feat: search bar in show

--- src/components/Search.js
+++ src/components/Search.js
@@ -8,13 +8,13 @@ export default function Search() {
     return (
         <React.Fragment>
             <div className="header">
+                
                 <p>Enter a country abbreviation to get information about covid-19</p>
             </div>
             <div className="search-box">
             <input
                onChange={(e) => setCountries(e.target.value)}
                 className="search-txt" type="text" name="" placeholder="Type br to search for Brazil" />
-
             
                 <a type="submit" className="search-btn" href={"show/"+countries}> 
                 <i class="fas fa-search"></i>
--- src/components/Show.js
+++ src/components/Show.js
@@ -1,33 +1,45 @@
-import React, { useState} from 'react';
+import React, {  useEffect, useState} from 'react';
 import './show.css'
 import api from '../services/api'
 
 export default function Show(props) {
 
     
-    const result = props.match.params.id
+  const result = props.match.params.id
 
-    const [cases, setCases] = useState([]);
+  const [cases, setCases] = useState([]);
+  const [location, setLocation] = useState('');
+  const [searchShow, setSearchShow] = useState('');
+  
 
-    const [location, setLocation] = useState('');
 
-
-    api.get(`countries/${result}/confirmed`).then((response) => {
+  
+  useEffect(() => {
+        api.get(`countries/${result}/confirmed`).then((response) => {
         setLocation(response.data[2].countryRegion)
     })
-
-   
-    
-        api.get(`countries/${result}/confirmed`).then((response) => {
+  
+  api.get(`countries/${result}/confirmed`).then((response) => {
         setCases(response.data)
-    })
+  })
+    
+    
+    
+  })
+  
+
+  
     
 
 
 
-    return (
-        <div class="table-container">
-            <h2 class="table-heading">Situation in {location}</h2>
+  return (
+        
+    <div className="table-container">
+      <input id="show-input" onChange={(ev) => setSearchShow(ev.target.value)} className="search-show" type="text" placeholder='Type "br" to search for Brazil'></input>
+      <a id="show-link" type="submit" href={searchShow} ><i class="fas fa-search"></i></a>
+      
+        <h2 className="table-heading">Situation in {location}</h2>
 
       <table>
         <thead>
@@ -44,7 +56,7 @@ export default function Show(props) {
 
         <tbody>
           {cases.map((caso) => (
-            <tr>
+            <tr key={caso.uid}>
                 <td data-heading="Country">{caso.countryRegion}</td>
                 <td data-heading="City">{caso.provinceState}</td>
                 <td data-heading="Confirmed cases">{caso.confirmed}</td>
