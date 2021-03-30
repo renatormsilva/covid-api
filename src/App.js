ab4754eda12dd3fc049331891019fd2c014bad82
renatormsilva <renato.brtkm@gmail.com>
2021-03-30 14:18:59 -0400

    wip

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import './App.css';
-import Routes from './routes'
+import { Helmet } from "react-helmet";
+import React from "react";
+import "./App.css";
+import Routes from "./routes";
 
 function App() {
   return (
-    <Routes/>
+    <>
+      <Routes />
+      <Helmet>
+        <meta property="og:image" content={process.env.REACT_APP_IMAGE} />
+      </Helmet>
+    </>
   );
 }
 
